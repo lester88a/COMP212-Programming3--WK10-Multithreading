e3e748f0770f48feabdf68ca30d0590b6b1683e9
lester88a <lester88a@gmail.com>
2015-11-18 09:39:43 -0500

    1

---
+++ Multithreading/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Threading;
+
+namespace Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*  Multithreading
+             *  ~execution path
+             *  
+             * 
+             * Life cycle: multithreading 
+             * Model
+             * 
+             * Abort:退出
+             * 
+             * Priority Levels:
+             *  1.priority Highest
+             *  2.above normal
+             *  3.normal
+             *  4.below normal
+             *  5. lowest
+             *  
+             * 
+             */
+
+            ThreadStart testTHreadStart = new ThreadStart(new Program().testThread);
+
+            Thread th = new Thread(testTHreadStart);
+
+            th.Start();
+
+            Console.ReadLine();
+        }
+
+        public void testThread()
+        { 
+            //executing the thread
+            int count = 0;
+            while (count++ <50 )
+            {
+                Console.WriteLine("Thread executed "+count+" times");
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
